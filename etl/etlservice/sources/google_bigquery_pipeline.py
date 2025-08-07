600de1f651941a259ffff012610dbf8c9d49d816
Ranjithsyncfusion <ranjithkumar.chinnadurai@syncfusion.com>
2025-08-07 09:00:06 +0000

    Added the latest Azure ARM Packages

--- etl/etlservice/sources/google_bigquery_pipeline.py
+++ etl/etlservice/sources/google_bigquery_pipeline.py
@@ -1,29 +1,30 @@
-from typing import List
-
 import dlt
-from dlt.sources.credentials import ConnectionStringCredentials
-from dlt.common import pendulum
-from sqlalchemy import create_engine
-from sql_database import sql_database, sql_table
-
+import pandas as pd
+from google.cloud import bigquery
 
 def load_standalone_table_resource() -> None:
-    """Load a few known tables with the standalone sql_table resource"""
+    credentials_path = "{1}"
+    project_id = "{2}"
+    dataset = "{5}"
+    table_names = [{4}]  
+
     pipeline = dlt.pipeline(
-        pipeline_name="{0}base", destination='{3}', dataset_name="{0}"
+        pipeline_name="{0}_pipeline",
+        destination="{3}",
+        dataset_name="{0}"
     )
 
-    engine = create_engine('bigquery://{2}', credentials_path='{1}')
-    table_names = "{4}".split(",")  # Split input string by comma
-    schema = "{5}"  # Assign schema once
+    for table in table_names:
+        df = load_bigquery_table(credentials_path, project_id, dataset, table)
+        resource = dlt.resource(df, name=table)
+        load_info = pipeline.run(resource, table_name=table, write_disposition="merge")
+        print(load_info)
 
-    for table_name in table_names:
-        table_name = table_name.strip()  # Remove any extra spaces
-        table = sql_table(engine, table=table_name, schema=schema)
-        info = pipeline.run(table, write_disposition="merge")
-
+def load_bigquery_table(credentials_path: str, project_id: str, dataset: str, table_name: str) -> pd.DataFrame:
+    client = bigquery.Client.from_service_account_json(credentials_path)
+    query = "SELECT * FROM `" + project_id + "." + dataset + "." + table_name + "`"
+    df = client.query(query).to_dataframe(create_bqstorage_client=True)
+    return df
 
 if __name__ == "__main__":
-
-    # Load tables with the standalone table resource
-     load_standalone_table_resource()
+    load_standalone_table_resource()
